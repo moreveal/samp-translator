c98a95c76e64c471828ef81aea3189c63fe1baca
Vyacheslav <71901974+moreveal@users.noreply.github.com>
2022-05-16 00:14:21 +0300

    Add files via upload

--- samp-translator.lua
+++ samp-translator.lua
@@ -17,7 +17,6 @@ local wm = require 'windows.message'
 -- additionaly
 local sampev = require 'samp.events'
 local imgui = require 'mimgui'
-local requests = require 'requests'
 local new, str, sizeof = imgui.new, ffi.string, ffi.sizeof
 -- variables
 local threads, textlabels, chatbubbles = {}, {}, {}
@@ -517,39 +516,40 @@ end
 -- other
 local effil = require 'effil'
 function asyncHttpRequest(method, url, args, resolve, reject)
-   local request_thread = effil.thread(function (method, url, args)
-      local result, response = pcall(requests.request, method, url, args)
-      if result then
-         response.json, response.xml = nil, nil
-         return true, response
-      else
-         return false, response
-      end
-   end)(method, url, args)
-   if not resolve then resolve = function() end end
-   if not reject then reject = function() end end
-   lua_thread.create(function()
-      local runner = request_thread
-      while true do
-         local status, err = runner:status()
-         if not err then
-            if status == 'completed' then
-               local result, response = runner:get()
-               if result then
-                  resolve(response)
-               else
-                  reject(response)
-               end
-               return
-            elseif status == 'canceled' then
-               return reject(status)
-            end
-         else
-            return reject(err)
-         end
-         wait(0)
-      end
-   end)
+    local request_thread = effil.thread(function (method, url, args)
+       local requests = require 'requests'
+       local result, response = pcall(requests.request, method, url, args)
+       if result then
+          response.json, response.xml = nil, nil
+          return true, response
+       else
+          return false, response
+       end
+    end)(method, url, args)
+    if not resolve then resolve = function() end end
+    if not reject then reject = function() end end
+    lua_thread.create(function()
+       local runner = request_thread
+       while true do
+          local status, err = runner:status()
+          if not err then
+             if status == 'completed' then
+                local result, response = runner:get()
+                if result then
+                   resolve(response)
+                else
+                   reject(response)
+                end
+                return
+             elseif status == 'canceled' then
+                return reject(status)
+             end
+          else
+             return reject(err)
+          end
+          wait(0)
+       end
+    end)
 end
 
 function char_to_hex(str)
